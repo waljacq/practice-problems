5f2241cc22f50e7d61791fb42b0c08aee9900727
Walter Jacquette <waltjacquette@gmail.com>
2020-05-16 20:16:57 -0500

    Update coins to include recursive solution

--- golang/coin-change/main.go
+++ golang/coin-change/main.go
@@ -3,29 +3,30 @@ package main
 import "fmt"
 
 func change(amount int, coins []int) int {
-
 	dp := make([]int, amount+1)
 	dp[0] = 1
-	// if amount == 0 {
-	//     return 1
-	// }
-
-	// if amount < 0 || len(coins) == 0 {
-	//     return 0
-	// }
 
 	for i := 0; i < len(coins); i++ {
 		for j := 1; j < len(dp); j++ {
 			if j-coins[i] >= 0 {
 				dp[j] += dp[j-coins[i]]
 			}
-			// change(amount - coins[len(coins)-1], coins)
-			// change(amount, coins[:len(coins)-1])
 		}
 	}
 
 	return dp[amount]
-	// return change(amount - coins[len(coins)-1], coins) + change(amount, coins[:len(coins)-1])
+}
+
+func changeRecursive(amount int, coins []int) int {
+	if amount == 0 {
+		return 1
+	}
+
+	if amount < 0 || len(coins) == 0 {
+		return 0
+	}
+
+	return change(amount-coins[len(coins)-1], coins) + change(amount, coins[:len(coins)-1])
 }
 
 func main() {
